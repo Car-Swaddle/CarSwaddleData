65a7ad29bc50037e46760dbad07a2185019b9d42
Kyle Kendall <kkendall33@gmail.com>
2020-09-18 02:20:20 -0600

    Stupdi

---
+++ README.md
@@ -0,0 +1,3 @@
+# CarSwaddleData
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import CarSwaddleDataTests
+
+var tests = [XCTestCaseEntry]()
+tests += CarSwaddleDataTests.allTests()
+XCTMain(tests)
