83304df361394e25ef44b39c194914f576f0c6c1
Kyle Kendall <kkendall33@gmail.com>
2020-09-19 02:28:53 -0600

    Update packages

--- Package.swift
+++ Package.swift
@@ -14,13 +14,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Authentication", url: "https://github.com/Car-Swaddle/", .branch("master")),
+        .package(name: "CarSwaddleNetworkRequest", url: "https://github.com/Car-Swaddle/CarSwaddleNetworkRequest", .branch("master")),
+        .package(name: "CarSwaddleStore", url: "https://github.com/Car-Swaddle/CarSwaddleStore", .branch("master")),
+        .package(name: "Disk", url: "https://github.com/saoudrizwan/Disk.git", .upToNextMajor(from: "0.6.4")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CarSwaddleData",
-            dependencies: []),
+            dependencies: ["Authentication", "CarSwaddleNetworkRequest", "CarSwaddleStore", "Disk"]),
         .testTarget(
             name: "CarSwaddleDataTests",
             dependencies: ["CarSwaddleData"]),
